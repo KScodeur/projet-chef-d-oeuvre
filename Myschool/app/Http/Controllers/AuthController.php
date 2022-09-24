f362777485c0441bfe429b6b513c6d72f38793a6
KScodeur <kinhosamuel2@gmail.com>
2022-09-24 18:43:07 +0000

    modification en index

--- Myschool/app/Http/Controllers/AuthController.php
+++ Myschool/app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
 {
     public function login()
     {
-        return view('auth.login');
+        return view('auth.index');
     }
     public function registration()
     {
